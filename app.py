c86b0572087ffd6b462e2d718b40e0a9da505801
Kevin Serra <kevindanielserra@gmail.com>
2026-01-13 17:57:25 -0500

    automate current year

--- app.py
+++ app.py
@@ -3,11 +3,13 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 import json
+from datetime import datetime
 
 @app.route('/')
 def index():
     with open('config.json', 'r') as f:
         data = json.load(f)
+    data['current_year'] = datetime.now().year
     return render_template('index.html', **data)
 
 if __name__ == '__main__':
